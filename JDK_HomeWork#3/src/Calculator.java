7021d32228296351a663a32616cd0388e76603ba
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2023-12-15 14:50:42 +0300

    Calculator.java

---
+++ JDK_HomeWork#3/src/Calculator.java
@@ -0,0 +1,30 @@
+package seminar3.hw;
+
+public class Calculator {
+
+    public static <N extends Number> Number sum(N a, N b) {
+        System.out.println( (a.getClass().getName()+"|"+b.getClass().getName()).replace("java.lang.",""));
+        if(a instanceof Double || b instanceof Double) return a.doubleValue()+b.doubleValue();
+        if(a instanceof Float|| b instanceof Float)  return a.floatValue()+b.floatValue();
+        return a.intValue()+b.intValue();
+    }
+
+    public static <N extends Number> Number multiply(N a, N b){
+        System.out.println( (a.getClass().getName()+"|"+b.getClass().getName()).replace("java.lang.",""));
+        if(a instanceof Double || b instanceof Double) return a.doubleValue()*b.doubleValue();
+        if(a instanceof Float|| b instanceof Float)  return a.floatValue()*b.floatValue();
+        return a.intValue()*b.intValue();
+    }
+    public static <N extends Number> Number divide(N a, N b){
+        System.out.println( (a.getClass().getName()+"|"+b.getClass().getName()).replace("java.lang.",""));
+        if(a instanceof Double || b instanceof Double) return a.doubleValue()/b.doubleValue();
+        if(a instanceof Float|| b instanceof Float)  return a.floatValue()/b.floatValue();
+        return a.intValue()/b.intValue();
+    }
+    public static <N extends Number> Number subtract(N a, N b){
+        System.out.println( (a.getClass().getName()+"|"+b.getClass().getName()).replace("java.lang.",""));
+        if(a instanceof Double || b instanceof Double) return a.doubleValue()-b.doubleValue();
+        if(a instanceof Float|| b instanceof Float)  return a.floatValue()-b.floatValue();
+        return a.intValue()-b.intValue();
+    }
+}
